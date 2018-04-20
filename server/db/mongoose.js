3c93a89acccf365eefc82cc72972b0fe2ab113bb
Amera Abdallah <mera.a.abd@gmail.com>
2018-04-20 17:04:32 +0200

    delete todo route

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-mongoose.connect( process.env.MONGODB_URI ||
+mongoose.connect(  process.env.MONGODB_URI ||
 'mongodb://localhost/TodoApp');
 //'mongodb://Amera:urdogurdog258258@ds147659.mlab.com:47659/todoapp'
 module.exports = {mongoose};
